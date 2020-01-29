135e2b237bfe9109b074175f22dc33d70f0cc1f5
Teun Schuur <teun983@gmail.com>
2020-01-29 15:04:22 +0100

    working

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ let dino;
 let cactus[];
 
 
-function preload(){
-
-}
-
 function setup() {
-
+  createCanvas(600,600);
+  dino = new Dino();
 }
 
 
 function draw() {
+  dino.update();
+  dino.render();
+}
 
+
+function keyPressed(){
+  if(key = " "){
+    dino.jump();
+  }
 }
