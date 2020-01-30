7a7d77f75d9dc0079c73ffa6b75a718e355f5b08
Teun Schuur <teun983@gmail.com>
2020-01-30 17:03:10 +0100

    user and ai ready!!!!

--- code/game.js
+++ code/game.js
@@ -37,12 +37,16 @@ class Game{
     this.dino.jump();
   }
 
+  isDeath(){
+    return this.dead;
+  }
+
   getInputs(){
     return [this.dino.x/width, this.dino.y/height, this.dino.dx/width*0.0733333, this.dino.dy/height*-0.22, this.dino.isJumping/2, this.dino.yVell/-34,
-      this.cactuses[0].x/width, this.cactuses[0].y/height, this.cactuses[0].dx/width*0.0333, this.cactuses[0].dy/height*-0.3, this.cactuses[0].speed/40,
-      this.cactuses[1].x/width, this.cactuses[1].y/height, this.cactuses[1].dx/width*0.0333, this.cactuses[1].dy/height*-0.3, this.cactuses[1].speed/40,
-      this.cactuses[2].x/width, this.cactuses[2].y/height, this.cactuses[2].dx/width*0.0333, this.cactuses[2].dy/height*-0.3, this.cactuses[2].speed/40,
-      this.cactuses[3].x/width, this.cactuses[3].y/height, this.cactuses[3].dx/width*0.0333, this.cactuses[3].dy/height*-0.3, this.cactuses[3].speed/40,
+      this.cactuses[0].x/(width + 400+this.cactuses[0].speed*10+width/4), this.cactuses[0].y/height, this.cactuses[0].dx/width*0.0333, this.cactuses[0].dy/height*-0.3, this.cactuses[0].speed/40,
+      this.cactuses[1].x/(width + 400+this.cactuses[1].speed*10+width/4), this.cactuses[1].y/height, this.cactuses[1].dx/width*0.0333, this.cactuses[1].dy/height*-0.3, this.cactuses[1].speed/40,
+      this.cactuses[2].x/(width + 400+this.cactuses[2].speed*10+width/4), this.cactuses[2].y/height, this.cactuses[2].dx/width*0.0333, this.cactuses[2].dy/height*-0.3, this.cactuses[2].speed/40,
+      this.cactuses[3].x/(width + 400+this.cactuses[3].speed*10+width/4), this.cactuses[3].y/height, this.cactuses[3].dx/width*0.0333, this.cactuses[3].dy/height*-0.3, this.cactuses[3].speed/40,
     ]
   }
 }
--- code/user.js
+++ code/user.js
@@ -1,10 +1,7 @@
 let dino;
 let cactuses = [];
 let highScore = 0;
-
-////
 let game;
-/////
 
 function preload(){
   song = loadSound('audio/megaman-music.mp3');
@@ -24,53 +21,23 @@ function setup() {
   song.loop(true);
   song.setVolume(0.4);
   createCanvas(displayWidth-20, displayHeight-200);
-  // createCanvas(1200, 500);
   reset();
 }
 
 function reset(){
   game = new Game(true);
-  // dino = new Dino();
-  // cactuses = [];
-  // cactuses.push(new Cactus(width+0));
-  // cactuses.push(new Cactus(width+(width/4)*1));
-  // cactuses.push(new Cactus(width+(width/4)*3));
-  // cactuses.push(new Cactus(width*2));
-
 }
 
 function draw() {
   if(!game.isDeath()){
     game.update()
   }
-  // if(dino.run){
-  //   background(200);
-  //   push()
-  //   fill(255)
-  //   textSize(30);
-  //
-  //   text("Score: "+round(dino.score), 40, 40)
-  //   pop()
-  //   dino.render();
-  //   for(var i = 0; i < cactuses.length; i++){
-  //     cactus = cactuses[i]
-  //     cactus.render();
-  //   }
-  //   for(var i = 0; i < cactuses.length; i++){
-  //     cactus = cactuses[i]
-  //     cactus.move();
-  //     cactus.collision(dino);
-  //   }
-  //   dino.update();
-  // }
 }
 
 function keyPressed(){
-  // dino.score = round(dino.score)
   console.log(game.dino.score)
   game.dino.score = round(game.dino.score)
   if(key == " "){
-    // dino.jump();
     game.jump();
   }
   if(key == " " && game.isDeath()){
